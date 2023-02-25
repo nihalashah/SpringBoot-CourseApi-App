14f4a171364ad9792efe5450fc38c901b17cef93
Nihal Shah <nihalashah@yahoo.com>
2023-02-25 19:29:26 +0530

    first-commit

---
+++ src/test/java/io/javabrains/CourseApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
